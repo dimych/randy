a834753c8ad9b541bf7dc3795de262b3306730b0
Randy <hongarandy@gmail.com>
2023-04-02 11:57:37 +0400

    lesson10

--- instagram-app/script.js
+++ instagram-app/script.js
@@ -1,33 +1,25 @@
 "use strict";
 
-const playlist = getPlaylist();
-runPlayer(playlist);
+const userbase = getUser();
+runPlayer(userbase);
 
-function runPlayer(playlist) {
+function runPlayer(userbase) {
   const elementsWrapper = getElements();
 
   elementsWrapper.searchButton.addEventListener("click", () => {
     const searchString = elementsWrapper.searchInput.value;
     refresh(elementsWrapper.resultDiv);
 
-    // extract this code to function
-    // let foundTracks = filterTracks(playlist, searchString)
-    let foundTracks = [];
+    let User = [];
 
-    for (let i = 0; i < playlist.length; i++) {
-      // if (isTrackFitTheCondition(playlist[i]))
-      if (
-        playlist[i].name.includes(searchString) ||
-        playlist[i].artist.includes(searchString)
-      ) {
-        //if (playlist[i].name.includes(searchString)) {
-        foundTracks.push(playlist[i]);
-        // console.log(playlist[i])
+    for (let i = 0; i < userbase.length; i++) {
+      if (userbase[i].name.startsWith(searchString)) {
+        User.push(userbase[i]);
       }
     }
 
-    // renderResult(foundTracks)
-    renderResult(foundTracks, elementsWrapper);
+    // renderResult(User)
+    renderResult(User, elementsWrapper);
   });
 }
 
@@ -35,18 +27,19 @@ function refresh(el) {
   el.innerHTML = "";
 }
 
-function renderResult(tracks, wrapper) {
-  if (tracks.length === 0) {
-    console.log("tracks not found!");
-    // elementWrapper.resultDiv.innerHTML...
-    wrapper.resultDiv.innerHTML = "tracks not found!";
+function renderResult(username, wrapper) {
+  if (username.length === 0) {
+    console.log("user not found!");
+    wrapper.resultDiv.innerHTML = "user not found!";
   } else {
-    for (let i = 0; i < tracks.length; i++) {
+    for (let i = 0; i < username.length; i++) {
       const newValue =
         wrapper.resultDiv.innerHTML +
-        `<li><audio controls src='../music/${tracks[i].mp3FileName}'></audio>${tracks[i].name} 
-${getLIkeImageString(tracks[i])}
-        </li>`;
+        `<li><img src='../assets/images/${
+          username[i].photoName
+        }' width="300" ></img>${username[i].name}${getLIkeImageString(
+          username[i]
+        )}</li>`;
       wrapper.resultDiv.innerHTML = newValue;
     }
   }
@@ -54,56 +47,33 @@ ${getLIkeImageString(tracks[i])}
 
 function getLIkeImageString(track) {
   if (track.addedToFavorite === true) {
-    return '<img src="../assets/images/pngtree-vector-like-icon-png-image_695769.jpg" alt="" style="width:50px;">'
+    return '<img src="../assets/images/pngtree-vector-like-icon-png-image_695769.jpg" alt="" style="width:50px;">';
   } else {
-    return '<img src="../assets/images/940_like_icon.jpg" alt="" style="width:50px;">'
+    return '<img src="../assets/images/940_like_icon.jpg" alt="" style="width:50px;">';
   }
 }
 
-// todo: put mp3 file name to all tracks
-function getPlaylist() {
-  const track1 = {
+function getUser() {
+  const user1 = {
     id: 1,
-    name: "Here comes your man",
-    releaseDate: 2015,
-    artist: "Pixies",
-    fileUrl: "",
-    price: 30,
+    name: "Randy",
+    likes: 198,
+    dateOfBirth: 2015,
     addedToFavorite: true,
-    mp3FileName: 'CandyShop.mp3'
+    photoName: "fotos-da-torre-de-paris.jpg",
   };
 
-  const track2 = {
+  const user2 = {
     id: 2,
-    name: "Rhinestone eyes",
+    name: "Dima",
+    likes: 201,
     releaseDate: 2012,
-    artist: "Gorillaz",
-    price: 4,
     addedToFavorite: false,
-    mp3FileName: 'HereComesYourMan.mp3'
+    photoName: "polaris-dream.jpg",
   };
 
-  const track3 = {
-    id: 3,
-    name: "Candy shop",
-    releaseDate: 2002,
-    artist: "50cent",
-    price: 3,
-    addedToFavorite: true,
-    mp3FileName: 'JustALilBit.mp3'
-  };
-  const track4 = {
-    id: 3,
-    name: "Candy shop 2",
-    releaseDate: 2020,
-    artist: "50cent",
-    price: 5,
-    addedToFavorite: true,
-    mp3FileName: 'RhinstoneEyes.mp3'
-  };
-
-  const playlist = [track1, track2, track3, track4];
-  return playlist;
+  const userbase = [user1, user2];
+  return userbase;
 }
 
 function getElements() {
@@ -117,6 +87,3 @@ function getElements() {
   };
   return wrapper;
 }
-
-// we want to add to favorite some track: we should button near the each track ADD TO FAVORITE
-// save favorite tracks inside local storage
