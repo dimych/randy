480e8f9f76b319c9e7ce914ecddb3e169fd65f77
Randy <hongarandy@gmail.com>
2023-05-29 15:45:39 +0400

    practice

--- react/instagram-ts/src/components/todolist/Practice.tsx
+++ react/instagram-ts/src/components/todolist/Practice.tsx
@@ -1,7 +1,19 @@
+import * as React from "react";
+
 export const Practice = () => {
+  const [count, setCount] = React.useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
-      <button type="button">Click Me</button>
+      <button type="button" onClick={handleClick}>
+        Click Me
+      </button>
+
+      {count}
     </div>
   );
 };
