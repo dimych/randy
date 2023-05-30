d5804e692d99b76010c4c4b387a5bfd3123f904d
Randy <hongarandy@gmail.com>
2023-05-30 19:05:11 +0400

    codde

--- react/instagram-ts/src/components/todolist/Practice.tsx
+++ react/instagram-ts/src/components/todolist/Practice.tsx
@@ -2,13 +2,20 @@ import { useState } from "react";
 
 export const Practice = () => {
   const [count, setCount] = useState(0);
+  const [title, setTitle] = useState("hello");
 
   return (
     <div>
       <button type="button" onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <h1>{count}</h1>
+      <h3>{count}</h3>
+      <input
+        value={title}
+        onChange={(event) => {
+          setTitle(event.currentTarget.value);
+        }}
+      />
     </div>
   );
 };
